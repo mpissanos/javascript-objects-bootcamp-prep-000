ea7077797a603d387ff92f25df3698eb4493202f
Manuel Pissanos <mpissano@gmail.com>
2018-08-21 20:23:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,5 @@
 var playlist = {'Every Time I Die' : 'New Black'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = 'songTitle';
-  return playlist;
-}
+  return playlist.artistName = 'songTitle'
+ }
